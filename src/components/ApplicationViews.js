6a30c3de68b0834eb0969a967f016ea59e65e0e0
Havey3 <45610955+Havey3@users.noreply.github.com>
2019-03-19 14:53:04 -0400

    fixed redirect

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 import React, { Component } from "react";
 import EventList from "./events/eventList"
 import EventBuilder from "./events/eventBuilder"
